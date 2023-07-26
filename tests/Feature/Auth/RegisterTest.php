86a17dcc36d21f0c429a1a1d813810924bc99160
blockpc <soporte.blockpc@gmail.com>
2023-07-25 20:55:14 -0400

    Se valida la ruta para registrar usuarios y el formulario. Se actualiza el archivo

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -5,33 +5,61 @@ it('can get a regiter form', function() {
         ->assertStatus(200);
 });
 
-it('can show errors when regiter new user', function() {
-    $this->post('/registro')
-        ->assertSessionHasErrors([
-            'name', 'email', 'password', 'firstname', 'lastname'
-        ]);
-});
+// it('can show errors when regiter new user', function() {
+//     $this->post('/registro')
+//         ->assertSessionHasErrors([
+//             'name', 'email', 'password', 'firstname', 'lastname'
+//         ]);
+// });
 
-it('can a regiter new user', function() {
-    $this->post('/registro', [
+it('can show errors when regiter new user')
+    ->post('/registro')
+    ->assertSessionHasErrors([
+        'name', 'email', 'password', 'firstname', 'lastname'
+    ]);
+
+// it('can a regiter new user', function() {
+//     $this->post('/registro', [
+//         'name' => 'test',
+//         'email' => 'test@mail.com',
+//         'password' => 'password',
+//         'password_confirmation' => 'password',
+//         'firstname' => 'first',
+//         'lastname' => 'last'
+//     ])
+//         ->assertRedirect('/sistema/dashboard')
+//         ->assertStatus(302);
+
+//     $this->assertDatabaseHas('users', [
+//         'name' => 'test',
+//         'email' => 'test@mail.com',
+//     ]);
+
+//     $this->assertDatabaseHas('profiles', [
+//         'firstname' => 'first',
+//         'lastname' => 'last'
+//     ]);
+// });
+
+it('can a regiter new user')
+    ->defer(function() {
+        $this->post('/registro', [
+            'name' => 'test',
+            'email' => 'test@mail.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+            'firstname' => 'first',
+            'lastname' => 'last'
+        ])
+        ->assertRedirect('/sistema/dashboard')
+        ->assertStatus(302);
+    })
+    ->assertDatabaseHas('users', [
         'name' => 'test',
         'email' => 'test@mail.com',
-        'password' => 'password',
-        'password_confirmation' => 'password',
+    ])
+    ->assertDatabaseHas('profiles', [
         'firstname' => 'first',
         'lastname' => 'last'
     ])
-        ->assertRedirect('/sistema/dashboard')
-        ->assertStatus(302);
-
-
-    $this->assertDatabaseHas('users', [
-        'name' => 'test',
-        'email' => 'test@mail.com',
-    ]);
-
-    $this->assertDatabaseHas('profiles', [
-        'firstname' => 'first',
-        'lastname' => 'last'
-    ]);
-});
+    ->assertAuthenticated();
